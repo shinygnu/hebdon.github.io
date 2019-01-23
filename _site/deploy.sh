ea2c933d44bfa29c46935432e63b40ad28581851
Chris Hebdon <chris.hebdon@yale.edu>
2019-01-22 19:12:23 -0500

    rebuilding site Tue Jan 22 19:12:23 EST 2019

---
+++ _site/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+jekyll build
+
+# Go To Public folder
+
+cd ~/shinyram.github.io
+
+# cp -av /home/chrishebdon/shinyram.github.io/* .
+
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+
